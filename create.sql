9bdffd4209ef7df6c57459afea15bf5b7af9aea6
s-nakao <shogo.nakao@crezit.jp>
2024-02-29 00:02:03 +0900

    test data 作成ツール作成

---
+++ create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE your_table_name (
+    myid SERIAL PRIMARY KEY,
+    named VARCHAR,
+    named_s VARCHAR(8),
+    x_flag BOOLEAN,
+    rate DOUBLE PRECISION,
+    score BIGINT ,
+    time TIMESTAMP,
+    purchase DATE
+);
